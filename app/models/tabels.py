128cd919801090e6feba2d5126c4675d8ce34eb3
gabrielgrc <gabriel.bsb98@gmail.com>
2021-12-06 19:32:13 -0300

    Environment variables created

--- app/models/tabels.py
+++ app/models/tabels.py
@@ -6,9 +6,8 @@ class Folder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
 
-    #aqui pensar sobre usar ou não, pois na doc. não aparece mais
-    #def __init__(self, name):
-    #   self.name = name
+    def __init__(self, name):
+        self.name = name
 
     def __repr__(self):
         return "<Folder %r>" % self.name
